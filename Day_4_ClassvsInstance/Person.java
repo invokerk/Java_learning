8c87520383c34fe1a915b308255e34105d1c7bc8
invokerk <invoker1771@gmail.com>
2017-03-28 15:22:21 +0530

    Day 4: Class vs. Instance

--- Day_4_ClassvsInstance/Person.java
+++ Day_4_ClassvsInstance/Person.java
@@ -6,15 +6,31 @@ public class Person {
   
 	public Person(int initialAge) {
   		// Add some more code to run some checks on initialAge
+		if(initialAge >= 0){
+		    age = initialAge;
+		} else {
+		    System.out.println("Age is not valid, setting age to 0.");
+		    age = 0;
+		}
 	}
 
 	public void amIOld() {
   		// Write code determining if this person's age is old and print the correct statement:
-        System.out.println(/*Insert correct print statement here*/);
+            String stage = "";
+	    if(age < 13){
+	        stage = "You are young.";
+            } else if (age >= 13 && age < 18){
+	        stage = "You are a teenager.";
+	    } else {
+                stage = "You are old.";
+	    }
+
+            System.out.println(stage);
 	}
 
 	public void yearPasses() {
   		// Increment this person's age.
+		age++;
 	}
 
 
